4e25417485beec0608d73d63635811276f9acf80
dboeckli <dboeckli@gmail.com>
2024-11-04 15:00:04 +0100

    project setup

---
+++ src/test/java/ch/springframeworkguru/spring6authserver/Spring6AuthServerApplicationTest.java
@@ -0,0 +1,13 @@
+package ch.springframeworkguru.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+  
+}
