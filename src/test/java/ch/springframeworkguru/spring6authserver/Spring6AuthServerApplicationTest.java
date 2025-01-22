93e2dc104fa88305caf158f0dea8a46b82bc6415
Dominique Boeckli <dboeckli@gmail.com>
2025-01-22 10:02:14 +0100

    refactor project and build (#13)

--- src/test/java/ch/springframeworkguru/spring6authserver/Spring6AuthServerApplicationTest.java
+++ src/test/java/ch/springframeworkguru/spring6authserver/Spring6AuthServerApplicationTest.java
@@ -1,13 +1,16 @@
 package ch.springframeworkguru.spring6authserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Slf4j
 class Spring6AuthServerApplicationTest {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Application...");
     }
   
 }
